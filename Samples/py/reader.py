53f699f995787c23b538355ee5addeab42922127
Maxime <maxime.papin@b-com.com>
2020-02-07 14:44:19 +0100

    feat: Streaming sensor data (image+pose) and visualization example with Python and OpenCV

---
+++ Samples/py/reader.py
@@ -0,0 +1,158 @@
+from cv2 import cv2
+import os
+import numpy as np
+import glob
+import shutil
+import argparse
+import matplotlib.pyplot as plt
+import imutils
+from recorder_console import parse_args, \
+    mkdir_if_not_exists, read_sensor_images, \
+    read_sensor_poses, synchronize_sensor_frames
+
+
+workspace_path = "C:/recordings"
+
+recording = "HoloLensRecording__2020_01_27__08_42_51"
+
+recording_path = os.path.join(workspace_path, recording)
+
+reconstruction_path = os.path.join(recording_path, "reconstruction")
+image_path = os.path.join(reconstruction_path, "images")
+
+camera_width = 640
+camera_height = 480
+camera_params = { # fx, fy, cx, cy, k1, k2, p1, p2
+    "vlc_ll": "450.072070 450.274345 320 240 "
+                "-0.013211 0.012778 -0.002714 -0.003603",
+    "vlc_lf": "448.189452 452.478090 320 240 "
+                "-0.009463 0.003013 -0.006169 -0.008975",
+    "vlc_rf": "449.435779 453.332057 320 240 "
+                "-0.000305 -0.013207 0.003258 0.001051",
+    "vlc_rr": "450.301002 450.244147 320 240 "
+                "-0.010926 0.008377 -0.003105 -0.004976",
+    # "pv": "1.7230957099738822e+04 1.7230957099738822e+04 7.035e+02 3.955e+02 "
+    #             "0 0 0 0", # TODO Check values
+    "pv": "3124.6464 3122.8344 86.555904 64.628136 "
+                "0 0 0 0", # TODO Check values
+}
+[[3.76120520, -0.00500542345, 0.0566976666,            0],
+ [         0,     6.65095186, 0.0159159899,            0],
+ [         0,              0,  -1.00502515, -0.100502513],
+ [         0,              0,           -1,            0]]
+
+scaler = lambda s: np.array([[s, 0, 0, 0],
+                             [0, s, 0, 0],
+                             [0, 0, s, 0],
+                             [0, 0, 0, 1]])
+
+translater = lambda x,y,z: np.array([[x],
+                                     [y],
+                                     [z],
+                                     [0]])
+
+#################################################################################
+
+def getCube(pos, scale):
+    cube = [ np.array([[i],[j],[k],[1]])
+            for i,j,k in [(.5, .5, -.5), (-.5, .5, .5), (.5, -.5, .5), (.5, .5, .5),
+                        (-.5, -.5, .5), (.5, -.5, -.5), (-.5, .5, -.5), (-.5, -.5, -.5)] ]
+
+    cube = list(map(lambda p: np.dot(scaler(scale), p), cube))
+    cube = list(map(lambda p: np.add(p, translater(pos[0], pos[1], pos[2])), cube))
+    return cube
+
+def getCameraParams(camera_name):
+    ''' Returns the intrinsic camera parameters matrix '''
+    # try:
+    camera_params_list = list(map(float, camera_params[camera_name].split()))
+    # except:
+    #     return np.identity(4)
+    fx, fy, cx, cy, _, _, _, _ = np.array(camera_params_list, dtype=np.double)
+    K = np.array([[fx,  0, cx, 0],
+                  [ 0, fy, cy, 0],
+                  [ 0,  0,  1, 0],
+                  [ 0,  0,  0, 1]])
+    return K
+
+def renderPose(img, pose, camera_name, height, width, cube):
+    K = getCameraParams(camera_name)
+    P = K @ pose # @ is matrix multiplication (numpy)
+    for p in cube:
+        uv = P @ p
+        u, v = uv[0]/uv[2], uv[1]/uv[2]
+        if 0 <= u and u <= height and 0 <= v and v <= width:
+            cv2.circle(img, (u, v), 10, (0,255,0), -1)
+    return img
+
+def merge(time_stamps, camera_names): # WIP, not useful for now
+    indexes = {}
+    for cam in camera_names:
+        indexes[cam] = 0
+    ref_cam = camera_names[0]
+    global_time_stamps = []
+    done = False
+    while not done:
+        ref_time_stamp = time_stamps[ref_cam][indexes[0]]
+        global_time_stamps.append(ref_time_stamp)
+        for cam in camera_names[1:]:
+            cam_time_stamp = time_stamps[cam][indexes[cam]]
+            if ref_time_stamp == cam_time_stamp:
+                indexes[cam] += 1
+            elif ref_time_stamp > cam_time_stamp:
+                global_time_stamps.append(cam_time_stamp)
+                    
+#################################################################################
+
+if __name__ == "__main__":
+    args = parse_args()
+    if "vlc" in args.ref_camera_name:
+        camera_names = ["vlc_ll", "vlc_lf", "vlc_rf", "vlc_rr"]
+        camera_width = 640
+        camera_height = 480
+    elif "pv" in args.ref_camera_name:
+        camera_names = ["pv"]
+        camera_width = 1280
+        camera_height = 720
+    elif "long_throw" in args.ref_camera_name:
+        camera_names = ["long_throw_depth", "long_throw_reflectivity"]
+        camera_width = 640
+        camera_height = 480
+    elif "short_throw" in args.ref_camera_name:
+        camera_names = ["short_throw_depth", "short_throw_reflectivity"]
+        camera_width = 640
+        camera_height = 480
+
+    frames, poses = synchronize_sensor_frames(
+        args, recording_path, image_path, camera_names)
+
+    # Draw Sample
+    cube = getCube([0.2, 0, -.75], 0.1)
+
+    # cube = list(map(lambda p: np.add(p, translater(0.5, -0.5, -1)), cube))
+
+    ## vlc cameras
+    for id in range(len(frames)):
+        frame = frames[id]
+        pose = poses[id]
+        # proj = projs[id]
+
+        if len(frame) == 0:
+            continue
+
+        final = np.zeros((camera_width, 0, 3), np.uint8)
+        for l in range(len(camera_names)):
+            try:
+                img = cv2.imread(os.path.join(image_path, frame[l]))
+            except:
+                img = np.zeros((camera_height, camera_width, 3), np.uint8)
+            img = renderPose(img, pose[l], camera_names[l], camera_height, camera_width, cube)
+
+            if "vlc" in camera_names[l]:
+                imgout = imutils.rotate_bound(img, 90)
+                final = cv2.hconcat((final, imgout))
+            else:
+                final = img
+        cv2.imshow('Pose Rendering', final)
+        cv2.waitKey((int) (1000/args.frame_rate))
+    cv2.destroyAllWindows()
---
+++ Samples/py/streamer.py
@@ -0,0 +1,110 @@
+#from websocket import create_connection
+
+# ws = create_connection("ws://10.10.255.241")
+
+import websockets
+import asyncio
+import imutils
+import base64
+import numpy as np
+import json
+from cv2 import cv2
+import time
+
+from reader import scaler, translater, getCube, renderPose
+
+camera_to_image = np.array( # flip axis for OpenCV camera model
+                        [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+
+def getPose(frame_to_origin, viewMatrix, projMatrix):
+    frame_to_origin = np.array(list(map(float, frame_to_origin.split(',')))).reshape(4,4).T
+    viewMatrix = np.array(list(map(float, viewMatrix.split(',')))).reshape(4,4).T
+    # projMatrix = np.array(list(map(float, projMatrix.split(',')))).reshape(4,4).T
+
+    if abs(np.linalg.det(frame_to_origin[:3,:3]) - 1) < 0.01:
+        return camera_to_image @ viewMatrix @ np.linalg.inv(frame_to_origin)
+    return np.zeros((4,4))
+
+async def streamData(websocket, path):
+    print("Connected to Hololens. Begin streaming data...")
+    running = True
+    fps = 0
+    begin, end = 0, 0
+    cube = getCube([0, 0, -0.75], 0.1)
+    while running:
+        begin = time.time()
+        try:    
+            msg = await websocket.recv()
+        except:
+            print("Connection Lost, resetting...")
+            running = False
+            cv2.destroyAllWindows()
+            break
+
+        data = json.loads(msg)
+        if data['topic'] != '/hololens':
+            continue
+        stream = data['msg']
+        timestamp = stream['timestamp']
+        height = stream['height']
+        width = stream['width']
+        leftData = stream['left64']
+        rightData = stream['right64']
+        leftF2O = stream['leftFrameToOrigin']
+        leftViewMatrix = stream['leftCameraViewTransform']
+        leftProjMatrix = stream['leftCameraProjectionTransform']
+        rightF2O = stream['rightFrameToOrigin']
+        rightViewMatrix = stream['rightCameraViewTransform']
+        rightProjMatrix = stream['rightCameraProjectionTransform']
+        # print(time.ctime(timestamp/1e7)[:-5])
+
+        # Left
+        leftImg = readb64(leftData)
+        leftPose = getPose(leftF2O, leftViewMatrix, leftProjMatrix)
+        leftImg = renderPose(leftImg, leftPose, "vlc_lf", height, width, cube)
+        leftImg = imutils.rotate_bound(leftImg, 90)
+        # Right
+        rightImg = readb64(rightData)
+        rightPose = getPose(rightF2O, rightViewMatrix, rightProjMatrix)
+        rightImg = renderPose(rightImg, rightPose, "vlc_rf", height, width, cube)
+        rightImg = imutils.rotate_bound(rightImg, 90)
+
+        stereo = cv2.hconcat((leftImg, rightImg))
+        cv2.putText(stereo, f"FPS: {fps}", (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0))
+        cv2.imshow("Stream", stereo)
+        cv2.waitKey(1)
+
+        end = time.time()
+        fps = (int) (1 / (end - begin))
+
+
+async def hello(websocket, path):
+    count = 0
+    while count < 3:
+        msg = await websocket.recv()
+        data = json.loads(msg)
+        if data['op'] == 'advertise':
+            print(f"< {data['op']}{data['topic']} -> {data['type']}")
+        else:
+            print(f"< {data['op']}{data['topic']} -> {data['msg']['data']}")
+        count += 1
+    await streamData(websocket, path)
+
+def readb64(base64_string):
+    nparr = np.frombuffer(base64.b64decode(base64_string), np.uint8)
+    nparr = np.reshape(nparr, (480, 640, 1))
+    return cv2.cvtColor(nparr, cv2.COLOR_GRAY2BGR)
+
+def readLog():
+    with open("log.txt", 'r') as logFile:
+        data = logFile.readlines()
+    frame = readb64(data[0])
+
+    cv2.imshow("oui", frame)
+    cv2.waitKey(0)
+
+if __name__ == "__main__":
+
+    start_server = websockets.serve(hello, "10.10.254.226", 9090)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
